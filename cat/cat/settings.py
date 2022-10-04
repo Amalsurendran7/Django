d6dca315d5d366d14fa5c2b058c570be700ac48e
Amalsurendran7 <109056327+Amalsurendran7@users.noreply.github.com>
2022-10-04 12:48:25 +0530

    commit

--- cat/cat/settings.py
+++ cat/cat/settings.py
@@ -165,9 +165,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-THUMBNAIL_PROCESSORS = (
-    'image_cropping.thumbnail_processors.crop_corners',
-) + thumbnail_settings.THUMBNAIL_PROCESSORS
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
