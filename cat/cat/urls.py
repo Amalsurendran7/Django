5207be3b12f703bd84cc3b2f0a61dc0a2137e955
Amalsurendran7 <109056327+Amalsurendran7@users.noreply.github.com>
2022-10-04 12:59:23 +0530

    commit

--- cat/cat/urls.py
+++ cat/cat/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +26,6 @@ urlpatterns = [
     path('coupons/',include('coupons.urls')),
     path('carts/',include('carts.urls')),
     path('orders/',include('orders.urls')),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+
 ]
