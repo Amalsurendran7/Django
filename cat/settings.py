7c5251368bf1062a89fa14717def795ac748c6cf
Amalsurendran7 <109056327+Amalsurendran7@users.noreply.github.com>
2023-02-18 11:26:59 +0530

    refgr

--- cat/settings.py
+++ cat/settings.py
@@ -69,7 +69,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'cat.urls'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
